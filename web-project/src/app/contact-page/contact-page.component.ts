7f97ab93b40dc9e26d4040559c35416f6b5ba283
unknown <tramntn19406@st.uel.edu.vn>
2022-05-30 10:44:20 +0700

    Update 3 page: Blog page, Contact Page, Support Page

--- web-project/src/app/contact-page/contact-page.component.ts
+++ web-project/src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { passwordValidator } from 'validators/check.validators';
+import { User } from '../models/user';
+import { MyserviceService } from '../myservice.service';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  userForm: any;
+  users:any;
+  user: User = new User();
+  errorMessage: String = ""
+
+  constructor(private _service:MyserviceService, private _toast:ToastrService,private _FormBuilder:FormBuilder ) {}
 
   ngOnInit(): void {
+    this.userForm =this._FormBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+      question: ['', [Validators.required, Validators.minLength(20)]],
+    })
+  }
+  submitData(form:NgForm){
+    console.log("Form data ", form.value)
+    console.log("Model: ",this.user)
+    if(this.user._id==null){ //insert
+      alert("Bạn đã gửi yêu cầu thành công")
+      this._service.postUser(this.user).subscribe(res=>{
+        let resData = JSON.parse(JSON.stringify(res));
+        if(resData.message ==="success"){
+          alert("Insert Success!")
+          this._toast.success("Inserted successfully!","SUCCESS")
+        }
+        else{
+          alert("Fail!")
+        }
+      })
+    }
+  }
+
+  get Name(){
+    return this.userForm.controls['name']
+  }
+  get Email(){
+    return this.userForm.controls['email']
+  }
+  get Phone(){
+    return this.userForm.controls['phone']
+  }
+  get Question(){
+    return this.userForm.controls['question']
   }
 
 }
